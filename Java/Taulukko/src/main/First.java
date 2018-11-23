40550ba939aac6a94bb6382f5d3688338130b232
ToukoPouko <juhoe.vahakangas@gmail.com>
2018-11-23 16:16:50 +0200

    Koti 2018.11.23 16:16

---
+++ Java/Taulukko/src/main/First.java
@@ -0,0 +1,18 @@
+package main;
+import java.util.*;
+
+public class First {
+
+	public static void main(String[] args) {
+		Map<String, Integer> person = new LinkedHashMap<String, Integer>();
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Anna nimi: ");
+		String name = input.next();
+		System.out.println("Anna ikä: ");
+		Integer age = input.nextInt();
+		person.put(name, age);
+
+	}
+
+}
