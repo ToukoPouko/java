3cc478ed53b1a542df712a6ce4ac4e8298c0321a
ToukoPouko <juhoe.vahakangas@gmail.com>
2018-11-23 21:08:17 +0200

    Koti 2018.11.23 21.08

--- Java/Taulukko/src/main/First.java
+++ Java/Taulukko/src/main/First.java
@@ -1,17 +1,20 @@
 package main;
 import java.util.*;
+import fi.jyu.mit.ohj2.Tiedosto;
 
 public class First {
 
 	public static void main(String[] args) {
 		Map<String, Integer> person = new LinkedHashMap<String, Integer>();
 		Scanner input = new Scanner(System.in);
+		String[] lines = Tiedosto.lueTiedosto("D:\\Ohjelmointi\\java\\Java\\Taulukko\\src\\main\\data.txt");
 		
 		System.out.println("Anna nimi: ");
 		String name = input.next();
 		System.out.println("Anna ikä: ");
 		Integer age = input.nextInt();
 		person.put(name, age);
+		
 
 	}
 
