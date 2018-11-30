9a47bb3369d23404c5780981276d42e79ccc1f32
ToukoPouko <juhoe.vahakangas@gmail.com>
2018-11-30 11:41:56 +0200

    Koulu 2018.11.30 11:42

--- Java/Taulukko/src/main/First.java
+++ Java/Taulukko/src/main/First.java
@@ -81,6 +81,17 @@ public class First {
 	}
 	
 	
+	public static void sorting() {
+		Scanner input = new Scanner(System.in);
+		String[] lines = Tiedosto.lueTiedosto("C:\\atk_vahakangas\\ohjelmointi\\Java\\Taulukko\\src\\main\\data.txt");
+		
+		java.util.Collections.sort(Arrays.asList(lines));
+		for(String s : lines) {
+			System.out.println(s);
+		}
+	}
+	
+	
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		while(true) {	
@@ -88,7 +99,8 @@ public class First {
 			System.out.println("2. Näytä henkilöt");
 			System.out.println("3. Poista henkilö");
 			System.out.println("4. Hae henkilöä");
-			System.out.println("5. Tietoa listasta");
+			System.out.println("5. Järjestä taulukko");
+			System.out.println("6. Tietoa listasta");
 			
 			Integer user = input.nextInt();
 					
@@ -111,6 +123,9 @@ public class First {
 					search();
 					break;
 				case 5:
+					sorting();
+					break;
+				case 6:
 					String[] lines2 = Tiedosto.lueTiedosto("C:\\atk_vahakangas\\ohjelmointi\\Java\\Taulukko\\src\\main\\data.txt");
 					System.out.println("Tiedostossa on tällä hetkellä " + lines2.length + " henkilöä");
 					break;
